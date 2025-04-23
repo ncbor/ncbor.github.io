ad87b874ae5ef41f47c444dedf376a329e731ac9
ncbor <nickborsari@gmail.com>
2025-04-23 02:05:47 -0300

    Formatação nav3

--- js/nav.js
+++
@@ -1,8 +0,0 @@
-// js/nav.js
-document.addEventListener("DOMContentLoaded", () => {
-  const toggle = document.querySelector(".nav-toggle");
-  if (!toggle) return;
-  toggle.addEventListener("click", () => {
-    document.body.classList.toggle("nav-open");
-  });
-});
